aa29165de9903cd85de15851f38ead73189cd518
Carloshpjacinto <carlo.henrique37@gmail.com>
2025-01-03 18:26:11 -0300

    first commit

---
+++ server.ts
@@ -0,0 +1,11 @@
+import express, {Express} from 'express';
+
+
+const app:Express = express();
+
+app.use(express.json());
+
+app.listen(3000, () => {
+
+    console.log(`Rodando o servidor na porta 3000`)
+})
