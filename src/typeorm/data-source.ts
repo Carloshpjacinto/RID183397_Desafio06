0e4cacf31198fc01336ee7d5670e5b4593b069d6
Carloshpjacinto <carlo.henrique37@gmail.com>
2025-01-06 03:50:54 -0300

    feature/criacao-migrations

--- src/typeorm/data-source.ts
+++ src/typeorm/data-source.ts
@@ -1,14 +1,16 @@
+import 'reflect-metadata'
 import 'dotenv/config';
 import { DataSource } from 'typeorm';
 
-const dbport:number = Number(process.env.DB_PORT);
+const dbport = Number(process.env.DB_PORT);
 
 export const AppDataSource = new DataSource({
 
     type: 'postgres',
     host: process.env.DB_HOST,
     port: dbport,
-    username: process.env.DB_DNCOMMERCE,
+    username: process.env.DB_USERNAME,
     password: process.env.DB_PASS,
-    database: process.env.NAME
+    database: process.env.DB_NAME,
+    migrations: [`./src/typeorm/migrations/*.{ts,js}`]
 });
---
+++ src/typeorm/migrations/1736143814932-AddIdClienteEmPedidos.ts
@@ -0,0 +1,31 @@
+import { MigrationInterface, QueryRunner, TableColumn, TableForeignKey } from "typeorm";
+
+export class AddIdClienteEmPedidos1736143814932 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.addColumn(
+            'pedidos',
+            new TableColumn({
+                name: 'id_cliente',
+                type: 'integer',
+                isNullable: true,
+            })
+        )
+        
+        await queryRunner.createForeignKey(
+            'pedidos',
+            new TableForeignKey({
+                name: 'PedidosClientes',
+                columnNames: ['id_cliente'],
+                referencedTableName: 'clientes',
+                referencedColumnNames: ['id'],
+                onDelete: 'SET NULL'
+            })
+        )
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropForeignKey('pedidos', 'PedidosClientes');
+        await queryRunner.dropColumn('pedidos', 'id_cliente')
+    }
+}
