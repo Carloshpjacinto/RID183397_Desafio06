786777aba51f5cba3e3e6c9b4296638e3d224d7e
Carloshpjacinto <carlo.henrique37@gmail.com>
2025-01-08 16:21:56 -0300

    ajustando-serializacao-pedido-vendas

--- src/modules/pedidos/controller/PedidoController.ts
+++ src/modules/pedidos/controller/PedidoController.ts
@@ -1,5 +1,7 @@
 import { Request, Response } from "express";
 import CriandoPedidoServece from "../services/CriandoPedidoServece";
+import { ClienteRepository } from "../../clientes/repositories/ClienteRepository";
+import { ProdutoRepository } from "../../produtos/repositories/ProdutoRepository";
 
 export default class PedidoController{
 
@@ -8,6 +10,20 @@ export default class PedidoController{
         const codPedido = Math.floor(Math.random() * 99999)
 
         const {qtd_produto_pedido, id_cliente, id_produto} = req.body;
+
+        const clienteRepository = await ClienteRepository.findOne({where: {id: id_cliente}})
+        
+        if(!clienteRepository){
+                
+            throw new Error("Produto não encontrado")
+        }
+
+        const produtoRepository = await ProdutoRepository.findOne({where: {id: id_produto}})
+        
+        if(!produtoRepository){
+                
+            throw new Error("Produto não encontrado")
+        }
 
         const criandoPedido = new CriandoPedidoServece();
 
@@ -19,6 +35,30 @@ export default class PedidoController{
             id_produto
         })
 
-        return res.status(201).json(pedido)
+        const serializacao = {
+
+            id: pedido.id,
+            codPedido: codPedido,
+
+            cliente: {
+                nomeCliente: clienteRepository.nome_cliente,
+                email: clienteRepository.email,
+                endereco: {
+                    logradouro: clienteRepository.logradouro,
+                    endereco: clienteRepository.endereco,
+                    cep: clienteRepository.cep,
+                    numeroEndereco: clienteRepository.numero_endereco
+                }
+            },
+
+            produto: {
+                qtd_produto_pedido,
+                nomeProduto: produtoRepository.nome_produto,
+                categoria: produtoRepository.categoria,
+                preco: produtoRepository.preco,
+                desconto: produtoRepository.desconto
+            }
+        }
+        return res.status(201).json(serializacao)
     }
 }
--- src/modules/vendas/controller/VendaController.ts
+++ src/modules/vendas/controller/VendaController.ts
@@ -40,33 +40,39 @@ export default class VendaController{
         const desconto_replace = desconto.replace('%', '')
         const desconto_numero = Number(desconto_replace)
         
-        const valorFinalVenda = preco - (preco * (desconto_numero / 100))
+        const valorFinalVenda = (preco - (preco * (desconto_numero / 100)) * pedido.qtd_produto_pedido)
 
         const criandoVenda = new CriandoVendaServece();
 
-        const serializacao = {
-
-            codVenda: codVenda,
-            nomeProduto: produto.nome_produto,
-            precoProduto: produto.preco,
-            descontoProduto: produto.desconto,
-            precoFinal: valorFinalVenda,
-            nomeCliente: cliente.nome_cliente,
-            email: cliente.email,
-            endereco: {
-                logradouro: cliente.logradouro,
-                rua: cliente.endereco,
-                numero: cliente.numero_endereco,
-                cep: cliente.cep
-            }
-        }
-
-        await criandoVenda.execute({
+        const venda = await criandoVenda.execute({
 
             cod_venda: codVenda,
             valor_venda: valorFinalVenda,
             id_pedido
         })
+
+        const serializacao = {
+
+            id: venda.id,
+            codVenda: codVenda,
+            produtos: {
+                nomeProduto: produto.nome_produto,
+                quantidadeProduto: pedido.qtd_produto_pedido,
+                precoProdutoUnidade: produto.preco,
+                descontoProdutos: produto.desconto,
+                precoFinal: valorFinalVenda,
+            },
+            cliente:{
+                nomeCliente: cliente.nome_cliente,
+                email: cliente.email,
+                endereco: {
+                    logradouro: cliente.logradouro,
+                    rua: cliente.endereco,
+                    numero: cliente.numero_endereco,
+                    cep: cliente.cep
+                }
+            }
+        }
 
         return res.status(201).json(serializacao)
     }
